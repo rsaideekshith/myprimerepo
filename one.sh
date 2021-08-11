de301261d14affc355f6f57c2de6020e36d45ed0
Sai Deekshith <rsaideekshith@gmail.com>
2021-08-11 10:47:13 +0530

    new file is added

---
+++ one.sh
@@ -0,0 +1,28 @@
+stage=0
+number=10
+while [ $stage -lt $number ]
+do
+	echo " Sai Deekshith has initiated learning DevOps  "
+	sleep 2
+	number=$((number - 2))
+echo " He is putting tireless and painstaking efforts to attain his target "
+sleep 2
+number=$((number - 2))
+	echo " He is relentlessly working on bash scripting  "
+	sleep 2
+     number=$((number - 2))
+     echo "The potentiality that he has got is incredible "
+     sleep 2
+     number=$((number - 2))
+     echo " This is really unimaginary    "
+     sleep 2
+     number=$((number - 2))
+done
+echo " All The Best Sai Deekshith!!!. Cheers up!!! "
+
+
+
+
+
+
+
